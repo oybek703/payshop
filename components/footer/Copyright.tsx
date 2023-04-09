c1a08ff525c1f9b37e3da87e0e3c9cb78b83a168
Yoriqulov Oybek <hhoybek@gmail.com>
2023-04-09 23:01:58 +0500

    footer finished
    commit 6e592a198d40e86d489d973324656ee5587ff64d
    Author: Yoriqulov Oybek <hhoybek@gmail.com>
    Date:   Sun Apr 9 23:01:41 2023 +0500
    
        responsive footer
    
    commit 7fe8de2615eef541321905810696e33220bce3a0
    Author: Yoriqulov Oybek <hhoybek@gmail.com>
    Date:   Sun Apr 9 22:39:36 2023 +0500
    
        footer copyright and payment
    
    commit c633af62f6391f0e198da2cc6cbf777e1b287b6d
    Author: Yoriqulov Oybek <hhoybek@gmail.com>
    Date:   Sun Apr 9 22:17:02 2023 +0500
    
        footer newsletter
    
    commit b72365815adaf619c553261428e3324c43b43655
    Author: Yoriqulov Oybek <hhoybek@gmail.com>
    Date:   Sun Apr 9 19:06:55 2023 +0500
    
        footer links start

---
+++ components/footer/Copyright.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './footer.module.scss'
+import Link from 'next/link'
+import { IoLocationSharp } from 'react-icons/io5'
+
+const data: { name: string; link: string }[] = [
+  { name: 'Privacy Center', link: '' },
+  { name: 'Privacy & Cookie Policy', link: '' },
+  { name: 'Manage Cookies', link: '' },
+  { name: 'Terms & Conditions', link: '' },
+  { name: 'Copyright Notice', link: '' }
+]
+
+const Copyright = () => {
+  return (
+    <div className={styles.footer__copyright}>
+      <section>&copy; PAYSHOP ALL RIGHTS RESERVED.</section>
+      <section>
+        <ul>
+          {data.map(({ link, name }) => (
+            <li key={name}>
+              <Link href={link}>{name}</Link>
+            </li>
+          ))}
+          <li>
+            <a>
+              <IoLocationSharp /> Uzbekistan
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export default Copyright
---
+++ components/footer/NewsLetter.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './footer.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const NewsLetter = () => {
+  return (
+    <div className={styles.footer__newsletter}>
+      <h3>SIGN UP FOR OUT NEWSLETTER</h3>
+      <div className={styles.footer__flex}>
+        <input type="text" placeholder="Your email address..." />
+        <button className={styles.btn__primary}>SUBSCRIBE</button>
+      </div>
+      <p>
+        By clicking the SUBSCRIBE button you are agreeing to
+        <Link href="/"> Our privacy & Cookie Policy</Link>
+      </p>
+    </div>
+  )
+}
+
+export default NewsLetter
